d6dc3bc21e973021b9c2c80281263222eee7c50c
Raylander Henrique <raylander.h.borges@gmail.com>
2023-03-13 22:33:56 -0300

    Aprendendo a usar git pelo terminal

---
+++ app.js
@@ -0,0 +1,15 @@
+function getRandom(min, max){
+    return Math.random() * (max - min) + min;
+
+}
+
+const x =  getRandom(5, 10);
+
+console.log(x)
+
+function a(t){
+    return Math.floor(t)
+}
+
+const y = a(76)
+console.log(y)
