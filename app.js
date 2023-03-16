5d901fc46c891aaff0be1a795fcb7d38c4ebc9c8
Raylander Henrique <raylander.h.borges@gmail.com>
2023-03-16 20:47:17 -0300

    Resolvendo alguns desafios

--- app.js
+++ app.js
@@ -13,3 +13,65 @@ function a(t){
 
 const y = a(76)
 console.log(y)
+
+
+
+// Template Literal (customização de Strings)
+
+const nome = 'Jean'
+
+const email = 
+
+`   Pai ${nome}, 
+
+eu não sou 'gay'
+
+obrigado, por entender`
+
+//Date
+
+
+const data2 = new Date('March 16 2023 8:30')
+
+
+// Desafios
+
+
+// Criar objeto endereco
+
+let endereco = {
+    rua : 'Jorge e Matheus, 395',
+    cidade : 'Los Santos',
+    cep : '34410-605',
+    
+
+  }
+
+
+  function exibirEndereco(endereco){
+    for(chave in endereco){
+        console.log(chave, endereco[chave])
+    }
+  }
+
+
+  //Igualdade de Objetos
+
+  function endereco(){
+    this.rua = rua,
+    this.cidade = cidade,
+    this.cep = cep
+  }
+
+  const endereco1 = new endereco('a', 'b', 'c')
+  const endereco2 = new endereco('a', 'b', 'c')
+
+  function saoIguais(endereco1, endereco2){
+    //propriedades do objeto
+
+  }
+
+  function saoMIguais(endereco1, endereco2){
+    //endereco de memoria
+
+  }
